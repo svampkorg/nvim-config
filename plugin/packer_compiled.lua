8d3fbdfb58063fa0962e80fe1420c7aa5dce5765
svampkorg <svampkorg@gmail.com>
2022-11-12 12:12:26 +0100

    loaded onedark

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -226,6 +226,11 @@ _G.packer_plugins = {
     path = "/Users/bobbo/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
+  ["onedark.nvim"] = {
+    loaded = true,
+    path = "/Users/bobbo/.local/share/nvim/site/pack/packer/start/onedark.nvim",
+    url = "https://github.com/navarasu/onedark.nvim"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/Users/bobbo/.local/share/nvim/site/pack/packer/start/packer.nvim",
