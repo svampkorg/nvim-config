61fa5a641aec2848432fbaea50863fb0b6485114
svampkorg <svampkorg@gmail.com>
2022-11-12 12:11:58 +0100

    added and using onedark colorscheme, instead of my custom githubdarkthing

--- lua/bobbo/core/colorscheme.lua
+++ lua/bobbo/core/colorscheme.lua
@@ -4,7 +4,7 @@
 --	print("Colorscheme not found!")
 --	return
 --end
---
+-- NIGHTFOX CUSTOM
 local options = {
 	--options
 	-- Compiled file's destination location
@@ -175,8 +175,60 @@ require("nightfox").setup({
 	--groups = groups,
 })
 
-local status_nightfox, _ = pcall(vim.cmd, "colorscheme nordfox")
-if not status_nightfox then
+-- local status_nightfox, _ = pcall(vim.cmd, "colorscheme nordfox")
+-- if not status_nightfox then
+-- 	print("Colorscheme not found!")
+-- 	return
+-- end
+
+-- ONEDARK
+local setup, onedark = pcall(require, "onedark")
+if not setup then
+	return
+end
+
+onedark.setup({
+	-- Main options --
+	style = "dark", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	transparent = false, -- Show/hide background
+	term_colors = true, -- Change terminal color as per the selected theme style
+	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+	-- toggle theme style ---
+	toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+	toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+	-- Change code style ---
+	-- Options are italic, bold, underline, none
+	-- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+	code_style = {
+		comments = "italic",
+		keywords = "none",
+		functions = "none",
+		strings = "italic",
+		variables = "none",
+	},
+
+	-- Lualine options --
+	lualine = {
+		transparent = false, -- lualine center bar transparency
+	},
+
+	-- Custom Highlights --
+	colors = {}, -- Override default colors
+	highlights = {}, -- Override highlight groups
+
+	-- Plugins Config --
+	diagnostics = {
+		darker = true, -- darker colors for diagnostic
+		undercurl = true, -- use undercurl instead of underline for diagnostics
+		background = true, -- use background color for virtual text
+	},
+})
+
+local check, _ = pcall(vim.cmd, "colorscheme onedark")
+if not check then
 	print("Colorscheme not found!")
 	return
 end
