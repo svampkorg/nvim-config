3eec6d3fc66e64351fd883c5a63d6d2651dec9b6
svampkorg <svampkorg@gmail.com>
2022-11-19 12:30:29 +0100

    something something dark side

--- lua/bobbo/plugins/nvim-tree.lua
+++ lua/bobbo/plugins/nvim-tree.lua
@@ -66,4 +66,15 @@ nvimtree.setup({
 			},
 		},
 	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		debounce_delay = 50,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
 })
